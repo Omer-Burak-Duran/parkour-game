9628dff3067f545354c7ea51ae5014b10bc858eb
Omer-Burak-Duran <omerburakduran@gmail.com>
2024-08-23 08:46:03 +0300

    Added Keyboard and Mouse inputs

--- ParkourGame/src/main/GamePanel.java
+++ ParkourGame/src/main/GamePanel.java
@@ -1,16 +1,47 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
-    public GamePanel() {
+    private MouseInputs mouseInputs;
+    private KeyboardInputs keyboardInputs;
+    private int xPos = 100;
+    private int yPos = 100;
+    private int width = 200;
+    private int height = 50;
 
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        keyboardInputs = new KeyboardInputs(this);
+        addKeyListener(keyboardInputs);
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+
+    public void changeXPos(int value) {
+        this.xPos += value;
+        repaint();
+    }
+
+    public void changeYPos(int value) {
+        this.yPos += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y) {
+        xPos = x;
+        yPos = y;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(100,100,200,50);
+        g.fillRect(xPos, yPos, width, height);
     }
 }
